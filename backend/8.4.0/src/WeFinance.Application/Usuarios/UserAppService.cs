f7327ec029ee1787e4013cd1e35ce6358ed491f5
Guilherme Medeiros <gsmedeiros3@ucs.br>
2025-03-17 18:04:30 -0300

    Início Application

---
+++ backend/8.4.0/src/WeFinance.Application/Usuarios/UserAppService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Application.Services;
+using Abp.Domain.Repositories;
+
+namespace WeFinance.Usuarios
+{
+    public class UserAppService : ApplicationService
+    {
+        private readonly IRepository<User, long> _userRepository;
+
+        public UserAppService(IRepository<User, long> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+    }
+}
--- backend/8.4.0/src/WeFinance.Core/Usuarios/User.cs
+++ backend/8.4.0/src/WeFinance.Core/Usuarios/User.cs
@@ -14,6 +14,7 @@ namespace WeFinance.Usuarios
         [MaxLength(100)]
         public string Name { get; set; }
         public string Email { get; set; }
+        public string Username { get; set; }
         public string PasswordHash { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
